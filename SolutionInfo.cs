5eca0ee750616850e59a461875dffc4f3a3be227
Vincent DARON <vdaron@ask.be>
2012-09-20 11:41:39 +0200

    Set Version number to 3.99 (alpha 4)

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyCompany("ASK sa")]
 [assembly: AssemblyProduct("ASK ServEasy")]
-[assembly: AssemblyCopyright("Copyright © ASK 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © ASK 2010-2012")]
 [assembly: AssemblyTrademark("")]
 
 // Version information for an assembly consists of the following four values:
@@ -15,5 +15,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.4.0")]
-[assembly: AssemblyFileVersion("3.2.4.0")]
+[assembly: AssemblyVersion("3.99.0.0")]
+[assembly: AssemblyFileVersion("3.99.0.0")]
