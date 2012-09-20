f5e26f6317246b8011a300d6a09134f9c875a4ff
Vincent DARON <vdaron@ask.be>
2012-09-20 11:34:57 +0200

    Release 3.3.5

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("ASK sa")]
-[assembly: AssemblyProduct("ASK ServEasy (log4net old)")]
+[assembly: AssemblyProduct("ASK ServEasy")]
 [assembly: AssemblyCopyright("Copyright © ASK 2010-2012")]
 [assembly: AssemblyTrademark("")]
 
@@ -15,5 +15,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.4.0")]
-[assembly: AssemblyFileVersion("3.2.4.0")]
+[assembly: AssemblyVersion("3.3.5.0")]
+[assembly: AssemblyFileVersion("3.3.5.0")]
